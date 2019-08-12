a7d2a78269648e21d7d310a781433ae136562225
Jake Shams <jshams62@gmail.com>
2019-08-12 10:54:34 -0700

    create population and create pop connections implemented

--- herd.py
+++ herd.py
@@ -43,18 +43,40 @@ class Simulation(object):
         self.total_infected = initial_infected
         self.current_infected = initial_infected
         self.total_dead = 0
-
-        # self.population = self._create_population(self.initial_infected)
+        # self._create_population()
+        # self._create_population_connections()
 
     def _create_population(self):
         '''creates a population with population_size(int) people
         Will create initial_infected(int) people infected
         Will also create about vacc_percentage(float 0-1) vaccinated people'''
-        pass
+        for i in range(self.population_size):
+            if i < self.initial_infected:
+                # create an infected person
+                infected_person = Person(i, False, True)
+                self.population_graph.add_vertex(infected_person)
+            else:  # create a healthy person
+                # if random float from 0 - 1 is less than the vacc pct
+                if random() < self.vacc_percentage:
+                    # create a healthy vaccinated person
+                    healthy_vaccinated_person = Person(i, True, False)
+                    self.population_graph.add_vertex(healthy_vaccinated_person)
+                else:
+                    # create a healthy unvaccinated person
+                    healthy_person = Person(i, False, False)
+                    self.population_graph.add_vertex(healthy_person)
 
     def _create_population_connections(self):
         '''Create random edges between already created vertices'''
-        pass
+        lst_of_people = [person for person in self.population_graph]
+        from math import log
+        avg_connections = int(log(10 * self.population_size, 10) ** 2)
+        for _ in range(self.population_size * avg_connections):
+            random_person = choice(lst_of_people)
+            other_random_person = choice(lst_of_people)
+            while other_random_person == random_person:
+                other_random_person = choice(lst_of_people)
+            self.population_graph.add_edge(random_person, other_random_person)
 
     def _simulation_should_continue(self):
         '''Returns a boolean indicating whether or not to continue simulation'''
@@ -88,3 +110,11 @@ class Simulation(object):
 
     def _infect_newly_infected(self):
         pass
+
+
+if __name__ == '__main__':
+    j = Simulation("Ebola", 100, 0.2,  0.5, 0.3, 10)
+    j._create_population()
+    j._create_population_connections()
+    print(j.population_graph.vertex_count)
+    print(j.population_graph.edge_count)
